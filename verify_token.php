0c22a11d0dc80725562670ac079b6d800cc82298
Edris Ibrahim <idrees@lemonadverts.com>
2016-09-07 15:15:29 +0400

    log facebook sent params to fb.txt

--- verify_token.php
+++ verify_token.php
@@ -2,7 +2,7 @@
 
 $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
-
+file_put_contents("fb.txt",file_get_contents("php://input"));
 if ($verify_token === 'my_token_code') {
 echo $challenge;
 }
