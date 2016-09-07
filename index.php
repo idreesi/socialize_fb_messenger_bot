649fb044407bd1e21a85e632922c0a4b7ac630e1
Edris Ibrahim <idrees@lemonadverts.com>
2016-09-07 17:19:37 +0400

    phpinput to POST

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 //$values = json_encode($_REQUEST) . json_encode($_GET) . json_encode($_POST) . json_encode(file_get_contents('php://input'));
-file_put_contents("fb.txt",json_encode($_POST));
+file_put_contents("fb.txt",json_encode($_REQUEST));
 ?>
