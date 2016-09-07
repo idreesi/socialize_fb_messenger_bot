eef878f38efbc175d295e41e841cca118ea2bbbe
Edris Ibrahim <idrees@lemonadverts.com>
2016-09-07 17:27:31 +0400

    phpinput to POST

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+print_r($_GET['hub_challenge']);
 //$values = json_encode($_REQUEST) . json_encode($_GET) . json_encode($_POST) . json_encode(file_get_contents('php://input'));
 file_put_contents("fb.txt",json_encode($_REQUEST));
 ?>
